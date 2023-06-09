7db0fda07010b365fa0b152cb593dbf52cb47c7b
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-06-08 23:44:52 -0300

    change: update controllers

--- PersonalFinances/Controllers/TransactionsController.cs
+++ PersonalFinances/Controllers/TransactionsController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using System.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.EntityFrameworkCore;
 using PersonalFinances.DataContext;
 
@@ -16,88 +18,175 @@ public class TransactionController : ControllerBase
    [HttpGet]
    public async Task<ActionResult<IEnumerable<object>>> ListTransactions()
    {
-      using (var context = new EFDataContext())
+      try
       {
-         var transactions = await context.Transactions.Select(e => new
+         using (var context = new EFDataContext())
          {
-            description = e.Description,
-            value = e.Value,
-            date = e.Date,
-            type = e.Type
-         })
-         .ToListAsync();
-         if (transactions == null)
-         {
-            return Ok(new { message = "No transactions" });
-         }
+            var transactions = await context.Transactions.Select(e => new
+            {
+               description = e.Description,
+               value = e.Value,
+               date = e.Date,
+               type = e.Type
+            }).ToListAsync();
 
-         return Ok(transactions);
-         /*
-                     var objs = new List<object>();
-                     foreach (var e in transactions)
-                     {
-                         var obj = new
-                         {
-                             description = e.description,
-                             value = e.value,
-                             date = e.date,
-                             type = e.type
-                         };
-                         objs.Add(obj);
-                     }
-                     return Ok(objs);*/
+            if (transactions == null)
+            {
+               return Ok(new { message = "No transactions" });
+            }
+
+            return Ok(transactions);
+         }
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
       }
    }
 
    [HttpPost]
    public async Task<ActionResult<TransactionModel>> AddTransaction(TransactionModel transaction)
    {
-      using (var context = new EFDataContext())
+      try
       {
-         context.Transactions.Add(transaction);
-         await context.SaveChangesAsync();
-
-         return Ok(transaction);
-      }
-   }
-
-   [HttpPut("{id}")]
-   public async Task<ActionResult<TransactionModel>> UpdateTransaction(int id, TransactionModel updatedTransaction)
-   {
-      using (var context = new EFDataContext())
-      {
-         var transaction = await context.Transactions.FindAsync(id);
-         if (transaction == null)
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
          {
-            return NotFound();
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
+            {
+               if (transaction != null)
+               {
+                  if (transaction.Description != null && transaction.Value != 0 && transaction.Type != null && transaction.Date != null && transaction.FkCategory != 0)
+                  {
+                     using (var context = new EFDataContext())
+                     {
+                        var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
+                        if (dbUser != null)
+                        {
+                           var newTransaction = new TransactionModel
+                           {
+                              Description = transaction.Description,
+                              Value = transaction.Value,
+                              Type = transaction.Type,
+                              Date = transaction.Date,
+                              FkUser = id,
+                              FkCategory = transaction.FkCategory
+                           };
+
+                           await context.Transactions.AddAsync(newTransaction);
+                           await context.SaveChangesAsync();
+
+                           var res = new
+                           {
+                              id = newTransaction.TransactionID,
+                              description = newTransaction.Description,
+                              value = newTransaction.Value,
+                              type = newTransaction.Type,
+                              date = newTransaction.Date,
+                              createdAt = newTransaction.CreatedAt,
+                              updatedAt = newTransaction.UpdatedAt,
+                              fkUser = newTransaction.FkUser,
+                              fkCategory = newTransaction.FkCategory
+                           };
+
+                           return Ok(res);
+                        }
+                        return BadRequest(new { error = "Failed to retrieve the user!" });
+                     }
+                  }
+               }
+               return NotFound(new { error = "No Data Recive." });
+            }
+            return Unauthorized(new { error = "Invalid Token!" });
          }
-
-         transaction.Description = updatedTransaction.Description;
-         transaction.Value = updatedTransaction.Value;
-         transaction.Date = updatedTransaction.Date;
-         transaction.Type = updatedTransaction.Type;
-
-         await context.SaveChangesAsync();
-
-         return Ok(transaction);
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
       }
    }
-   [HttpDelete("{id}")]
+
+   [HttpPatch]
+   public async Task<ActionResult<TransactionModel>> UpdateTransaction(TransactionModel updatedTransaction)
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
+            {
+               if (updatedTransaction != null)
+               {
+                  if (updatedTransaction.TransactionID != 0 && updatedTransaction.Description != null && updatedTransaction.Value != 0 && updatedTransaction.Type != null && updatedTransaction.Date != null && updatedTransaction.FkCategory != 0)
+                  {
+                     using (var context = new EFDataContext())
+                     {
+                        var dbTransaction = await context.Transactions.FindAsync(updatedTransaction.TransactionID);
+
+                        if (dbTransaction != null && dbTransaction.FkUser == id)
+                        {
+                           dbTransaction.Description = updatedTransaction.Description;
+                           dbTransaction.Value = updatedTransaction.Value;
+                           dbTransaction.Type = updatedTransaction.Type;
+                           dbTransaction.Date = updatedTransaction.Date;
+                           dbTransaction.FkCategory = updatedTransaction.FkCategory;
+
+                           await context.SaveChangesAsync();
+
+                           return Ok(dbTransaction);
+                        }
+                        return BadRequest(new { error = "Failed!" });
+                     }
+                  }
+               }
+               return NotFound(new { error = "No Data Recive." });
+            }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return BadRequest(new { error = "No token!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
+
+   [HttpDelete]
    public async Task<ActionResult<TransactionModel>> DeleteTransaction(int id)
    {
-      using (var context = new EFDataContext())
+      try
       {
-         var transaction = await context.Transactions.FindAsync(id);
-         if (transaction == null)
+         using (var context = new EFDataContext())
          {
-            return NotFound();
+            var transaction = await context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+               return NotFound();
+            }
+
+            context.Transactions.Remove(transaction);
+            await context.SaveChangesAsync();
+
+            return Ok(transaction);
          }
-
-         context.Transactions.Remove(transaction);
-         await context.SaveChangesAsync();
-
-         return Ok(transaction);
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
       }
    }
-
 }
--- PersonalFinances/Controllers/UserController.cs
+++ PersonalFinances/Controllers/UserController.cs
@@ -10,6 +10,7 @@ using System.Security.Claims;
 using DotNetEnv;
 
 using PersonalFinances.DataContext;
+using PersonalFinances.Services;
 using PersonalFinances.Models;
 
 namespace PersonalFinances.Controllers;
@@ -18,164 +19,167 @@ namespace PersonalFinances.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
-    [HttpGet("verify")]
-    public async Task<IActionResult> Verify()
-    {
-        if (HttpContext.Request.Headers["Authorization"] != String.Empty)
-        {
+   [HttpGet("verify")]
+   public async Task<IActionResult> Verify()
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
             var token = HttpContext.Request.Headers["Authorization"];
 
             var jwtToken = new JwtSecurityToken(token);
             var payload = jwtToken.Payload;
 
-
             if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-                using (var context = new EFDataContext())
-                {
-                    var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
+               using (var context = new EFDataContext())
+               {
+                  var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
 
-                    if (dbUser != null)
-                    {
-                        var res = new
-                        {
-                            id = dbUser.UserID,
-                            name = dbUser.Name,
-                            lastname = dbUser.LastName,
-                            email = dbUser.Email,
-                            photo = dbUser.Photo
-                        };
-
-                        return Ok(new { user = res });
-                    }
-                    else
-                        /*throw new ArgumentNullException("No user found.");*/
-                        return NotFound(new { error = "No user found." });
-                }
-            }
-            return Unauthorized(new { error = "Invalid Token!" });
-        }
-        else
-        {
-            return Unauthorized(new { error = "No token!" });
-        }
-    }
-
-    [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] UserModel user)
-    {
-        try
-        {
-            using (var context = new EFDataContext())
-            {
-                Env.Load();
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("SECRET_KEY") ?? throw new NullReferenceException());
-
-                var dbUser = await context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
-                if (user != null && user.Password != null && dbUser != null && dbUser.Password != null && dbUser.Email != null && dbUser.UserID != null)
-                {
-                    if (!PasswordUtility.VerifyPassword(user.Password, dbUser.Password))
-                        return Unauthorized(new { error = "Email/Password is wrong!" });
-
-                    var tokenDescriptor = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity(new[]
-                        {
-                            new Claim("id", dbUser.UserID.ToString()),
-                            new Claim("email", dbUser.Email),
-                        }),
-                        Expires = DateTime.UtcNow.AddDays(30),
-                        SigningCredentials = new SigningCredentials(
-                            new SymmetricSecurityKey(key),
-                            SecurityAlgorithms.HmacSha256Signature)
-                    };
-
-                    var token = tokenHandler.CreateToken(tokenDescriptor);
-                    var tokenString = tokenHandler.WriteToken(token);
-
-                    var res = new
-                    {
+                  if (dbUser != null)
+                  {
+                     var res = new
+                     {
                         id = dbUser.UserID,
                         name = dbUser.Name,
-                        lastname = dbUser.LastName,
+                        lastName = dbUser.LastName,
                         email = dbUser.Email,
                         photo = dbUser.Photo
-                    };
+                     };
 
-                    Response.Headers.Add("Authorization", tokenString);
-                    return Ok(new { user = res });
-                }
-                else
-                {
-                    return BadRequest(new { error = "Failed to retrieve the user!" });
-                }
+                     return Ok(new { user = res });
+                  }
+                  return NotFound(new { error = "No user found." });
+               }
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"[Server] Error: {ex.Message}");
-            return StatusCode(500, new { error = "Internal Server Error" });
-        }
-    }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
 
-    [HttpPost("register")]
-    public async Task<ActionResult<UserModel>> RegisterUser()
-    {
-        var stream = HttpContext.Request.Body;
-        var buffer = new byte[Convert.ToInt32(HttpContext.Request.ContentLength)];
-        await stream.ReadAsync(buffer, 0, buffer.Length);
-        var json = Encoding.UTF8.GetString(buffer);
-        var user = JsonConvert.DeserializeObject<UserModel>(json);
-        if (user != null && user.Password != null)
-        {
+   [HttpPost("login")]
+   public async Task<IActionResult> Login([FromBody] UserModel user)
+   {
+      try
+      {
+         using (var context = new EFDataContext())
+         {
+            Env.Load();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("SECRET_KEY") ?? throw new NullReferenceException());
+
+            var dbUser = await context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (user != null && user.Password != null && dbUser != null && dbUser.Password != null && dbUser.Email != null && dbUser.UserID != null)
+            {
+               if (!PasswordUtility.VerifyPassword(user.Password, dbUser.Password))
+                  return Unauthorized(new { error = "Email/Password is wrong!" });
+
+               var tokenDescriptor = new SecurityTokenDescriptor
+               {
+                  Subject = new ClaimsIdentity(new[]
+                  {
+                     new Claim("id", dbUser.UserID.ToString() ?? throw new ArgumentNullException()),
+                     new Claim("email", dbUser.Email),
+                  }),
+                  Expires = DateTime.UtcNow.AddDays(30),
+                  SigningCredentials = new SigningCredentials(
+                     new SymmetricSecurityKey(key),
+                     SecurityAlgorithms.HmacSha256Signature)
+               };
+
+               var token = tokenHandler.CreateToken(tokenDescriptor);
+               var tokenString = tokenHandler.WriteToken(token);
+
+               var res = new
+               {
+                  id = dbUser.UserID,
+                  name = dbUser.Name,
+                  lastName = dbUser.LastName,
+                  email = dbUser.Email,
+                  photo = dbUser.Photo
+               };
+
+               Response.Headers.Add("Authorization", tokenString);
+               return Ok(new { user = res });
+            }
+            return BadRequest(new { error = "Failed to retrieve the user!" });
+         }
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
+
+   [HttpPost("register")]
+   public async Task<ActionResult<UserModel>> RegisterUser()
+   {
+      try
+      {
+         var stream = HttpContext.Request.Body;
+         var buffer = new byte[Convert.ToInt32(HttpContext.Request.ContentLength)];
+         await stream.ReadAsync(buffer, 0, buffer.Length);
+         var json = Encoding.UTF8.GetString(buffer);
+         var user = JsonConvert.DeserializeObject<UserModel>(json);
+         if (user != null && user.Password != null)
+         {
             using (var context = new EFDataContext())
             {
-                var existingUser = await context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+               var existingUser = await context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
 
-                if (existingUser == null)
-                {
-                    string hashedPassword = string.Empty; PasswordUtility.HashPassword(user.Password);
+               if (existingUser == null)
+               {
+                  string hashedPassword = PasswordUtility.HashPassword(user.Password);
 
-                    var newUser = new UserModel
-                    {
-                        Name = user.Name,
-                        LastName = user.LastName,
-                        Email = user.Email,
-                        Password = hashedPassword
-                    };
+                  var newUser = new UserModel
+                  {
+                     Name = user.Name,
+                     LastName = user.LastName,
+                     Email = user.Email,
+                     Password = hashedPassword
+                  };
 
-                    context.Users.Add(newUser);
-                    await context.SaveChangesAsync();
+                  context.Users.Add(newUser);
+                  await context.SaveChangesAsync();
 
-                    var res = new
-                    {
-                        id = newUser.UserID,
-                        name = newUser.Name,
-                        lastname = newUser.LastName,
-                        email = newUser.Email,
-                        photo = newUser.Photo
-                    };
+                  var res = new
+                  {
+                     id = newUser.UserID,
+                     name = newUser.Name,
+                     lastName = newUser.LastName,
+                     email = newUser.Email,
+                     photo = newUser.Photo
+                  };
 
-                    return Ok(res);
-                }
-                else
-                {
-                    return BadRequest(new { error = "User is already registered!" });
-                }
+                  return Ok(res);
+               }
+               return BadRequest(new { error = "User is already registered!" });
             }
-        }
-        else
-        {
-            return BadRequest(new { error = "Unable to extract user!" });
-        }
-    }
+         }
+         return BadRequest(new { error = "Unable to extract user!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
 
-    [HttpPatch()]
-    public async Task<ActionResult<UserModel>> UpdateUser([FromBody] UserModel updatedUser)
-    {
-        if (HttpContext.Request.Headers["Authorization"] != String.Empty)
-        {
+   [HttpPatch()]
+   public async Task<ActionResult<UserModel>> UpdateUser([FromBody] UserModel updatedUser)
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
             var token = HttpContext.Request.Headers["Authorization"];
 
             var jwtToken = new JwtSecurityToken(token);
@@ -183,81 +187,80 @@ public class UserController : ControllerBase
 
             if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-                using (var context = new EFDataContext())
-                {
-                    var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
+               using (var context = new EFDataContext())
+               {
+                  var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
 
-                    if (dbUser != null)
-                    {
-                        dbUser.UserID = id;
-                        dbUser.Name = updatedUser.Name;
-                        dbUser.LastName = updatedUser.LastName;
-                        dbUser.Email = updatedUser.Email;
-                        dbUser.Password = updatedUser.Password;
-                        dbUser.Photo = updatedUser.Photo;
+                  if (dbUser != null)
+                  {
+                     dbUser.Name = updatedUser.Name;
+                     dbUser.LastName = updatedUser.LastName;
+                     dbUser.Email = updatedUser.Email;
+                     dbUser.Password = updatedUser.Password;
+                     dbUser.Photo = updatedUser.Photo;
 
-                        await context.SaveChangesAsync();
+                     await context.SaveChangesAsync();
 
-                        var res = new
-                        {
-                            id = dbUser.UserID,
-                            name = dbUser.Name,
-                            lastname = dbUser.LastName,
-                            email = dbUser.Email,
-                            photo = dbUser.Photo
-                        };
+                     var res = new
+                     {
+                        id = dbUser.UserID,
+                        name = dbUser.Name,
+                        lastName = dbUser.LastName,
+                        email = dbUser.Email,
+                        photo = dbUser.Photo
+                     };
 
-                        return Ok(new { user = res });
-                    }
-                    else
-                        /*throw new ArgumentNullException("No user found.");*/
-                        return NotFound(new { error = "No user found." });
-                }
+                     return Ok(res);
+                  }
+                  return NotFound(new { error = "No user found." });
+               }
             }
-            else
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
+
+   [HttpDelete]
+   public async Task<ActionResult<UserModel>> DeleteUser()
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-                return Unauthorized(new { error = "Invalid Token!" });
+               using (var context = new EFDataContext())
+               {
+                  var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
+
+                  if (dbUser != null)
+                  {
+                     context.Users.Remove(dbUser);
+                     await context.SaveChangesAsync();
+                     return Ok();
+                  }
+                  return NotFound(new { error = "No user found." });
+               }
             }
-        }
-        else
-        {
-            return Unauthorized(new { error = "No token!" });
-        }
-    }
-
-    [HttpDelete]
-    public async Task<ActionResult<UserModel>> DeleteUser()
-    {
-        var token = string.Empty;
-        try
-        {
-            token = HttpContext.Request.Headers["Authorization"];
-        }
-        catch (NullReferenceException)
-        {
-            return Unauthorized(new { error = "No token!" });
-        }
-
-        var jwtToken = new JwtSecurityToken(token);
-        var payload = jwtToken.Payload;
-
-        if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
-        {
-            using (var context = new EFDataContext())
-            {
-                var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
-
-                if (dbUser != null)
-                {
-                    context.Users.Remove(dbUser);
-                    await context.SaveChangesAsync();
-                    return Ok();
-                }
-                else
-                    /*throw new ArgumentNullException("No user found.");*/
-                    return NotFound(new { error = "No user found." });
-            }
-        }
-        return Unauthorized(new { error = "Invalid Token!" });
-    }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return BadRequest(new { error = ex.Message });
+      }
+   }
 }
