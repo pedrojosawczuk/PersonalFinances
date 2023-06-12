6be58b0f51cf172a1fb912edb5fe550813fba880
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-06-12 20:45:28 -0300

    change: Transaction Controller

--- PersonalFinances/Controllers/TransactionsController.cs
+++ PersonalFinances/Controllers/TransactionsController.cs
@@ -20,28 +20,204 @@ public class TransactionController : ControllerBase
    {
       try
       {
-         using (var context = new EFDataContext())
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
          {
-            var transactions = await context.Transactions.Select(e => new
-            {
-               description = e.Description,
-               value = e.Value,
-               date = e.Date,
-               type = e.Type
-            }).ToListAsync();
+            var token = HttpContext.Request.Headers["Authorization"];
 
-            if (transactions == null)
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-               return Ok(new { message = "No transactions" });
+               using (var context = new EFDataContext())
+               {
+                  List<TransactionModel> allTransactions = new List<TransactionModel>();
+                  var transactions = await context.Transactions
+                     .Where(e => e.FkUser == id)
+                     .Select(e => new
+                     {
+                        id = e.TransactionID,
+                        description = e.Description,
+                        value = e.Value,
+                        date = e.Date,
+                        type = e.Type,
+                        category = e.FkCategory
+                     })
+                     .ToListAsync();
+
+                  if (transactions == null)
+                  {
+                     return Ok(new { message = "No transactions" });
+                  }
+                  return Ok(new { transaction = transactions });
+               }
             }
-
-            return Ok(transactions);
+            return Unauthorized(new { error = "Invalid Token!" });
          }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
       }
       catch (Exception ex)
       {
          Console.WriteLine($"[Server] Error: {ex.Message}");
+         return StatusCode(500, new { error = ex.Message });
+      }
+   }
+
+   [HttpGet("income")]
+   public async Task<ActionResult<IEnumerable<object>>> ListIncome()
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
+            {
+               using (var context = new EFDataContext())
+               {
+                  List<TransactionModel> allTransactions = new List<TransactionModel>();
+                  var transactions = await context.Transactions
+                     .Where(e => e.FkUser == id && e.Type == "I")
+                     .Select(e => new
+                     {
+                        id = e.TransactionID,
+                        description = e.Description,
+                        value = e.Value,
+                        date = e.Date,
+                        type = e.Type,
+                        category = e.FkCategory
+                     })
+                     .ToListAsync();
+
+                  if (transactions == null)
+                  {
+                     return Ok(new { message = "No Income" });
+                  }
+                  return Ok(new { transaction = transactions });
+               }
+            }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (ArgumentException ex)
+      {
          return BadRequest(new { error = ex.Message });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return StatusCode(500, new { error = ex.Message });
+      }
+   }
+   [HttpGet("expenses")]
+   public async Task<ActionResult<IEnumerable<object>>> ListExpenses()
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
+            {
+               using (var context = new EFDataContext())
+               {
+                  List<TransactionModel> allTransactions = new List<TransactionModel>();
+                  var transactions = await context.Transactions
+                     .Where(e => e.FkUser == id && e.Type == "E")
+                     .Select(e => new
+                     {
+                        id = e.TransactionID,
+                        description = e.Description,
+                        value = e.Value,
+                        date = e.Date,
+                        type = e.Type,
+                        category = e.FkCategory
+                     })
+                     .ToListAsync();
+
+                  if (transactions == null)
+                  {
+                     return Ok(new { message = "No Expenses" });
+                  }
+                  return Ok(new { transaction = transactions });
+               }
+            }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return StatusCode(500, new { error = ex.Message });
+      }
+   }
+   [HttpGet("category/{categoryId}")]
+   public async Task<ActionResult<IEnumerable<object>>> ListExpenses(long categoryId)
+   {
+      try
+      {
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
+         {
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
+            {
+               using (var context = new EFDataContext())
+               {
+                  List<TransactionModel> allTransactions = new List<TransactionModel>();
+                  var transactions = await context.Transactions
+                     .Where(e => e.FkUser == id && e.FkCategory == categoryId)
+                     .Select(e => new
+                     {
+                        id = e.TransactionID,
+                        description = e.Description,
+                        value = e.Value,
+                        date = e.Date,
+                        type = e.Type,
+                        category = e.FkCategory
+                     })
+                     .ToListAsync();
+
+                  if (transactions == null)
+                  {
+                     return Ok(new { message = "No Transaction" });
+                  }
+                  return Ok(new { transaction = transactions });
+               }
+            }
+            return Unauthorized(new { error = "Invalid Token!" });
+         }
+         return Unauthorized(new { error = "No token!" });
+      }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"[Server] Error: {ex.Message}");
+         return StatusCode(500, new { error = ex.Message });
       }
    }
 
@@ -59,62 +235,45 @@ public class TransactionController : ControllerBase
 
             if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-               if (transaction != null)
+               using (var context = new EFDataContext())
                {
-                  if (transaction.Description != null && transaction.Value != 0 && transaction.Type != null && transaction.Date != null && transaction.FkCategory != 0)
+                  var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
+                  if (dbUser != null)
                   {
-                     using (var context = new EFDataContext())
-                     {
-                        var dbUser = await context.Users.FirstOrDefaultAsync(u => u.UserID == id);
-                        if (dbUser != null)
-                        {
-                           var newTransaction = new TransactionModel
-                           {
-                              Description = transaction.Description,
-                              Value = transaction.Value,
-                              Type = transaction.Type,
-                              Date = transaction.Date,
-                              FkUser = id,
-                              FkCategory = transaction.FkCategory
-                           };
+                     var newTransaction = new TransactionModel(
+                        transaction.Description,
+                        transaction.Value,
+                        transaction.Type,
+                        transaction.Date,
+                        id,
+                        transaction.FkCategory
+                     );
 
-                           await context.Transactions.AddAsync(newTransaction);
-                           await context.SaveChangesAsync();
+                     await context.Transactions.AddAsync(newTransaction);
+                     await context.SaveChangesAsync();
 
-                           var res = new
-                           {
-                              id = newTransaction.TransactionID,
-                              description = newTransaction.Description,
-                              value = newTransaction.Value,
-                              type = newTransaction.Type,
-                              date = newTransaction.Date,
-                              createdAt = newTransaction.CreatedAt,
-                              updatedAt = newTransaction.UpdatedAt,
-                              fkUser = newTransaction.FkUser,
-                              fkCategory = newTransaction.FkCategory
-                           };
-
-                           return Ok(res);
-                        }
-                        return BadRequest(new { error = "Failed to retrieve the user!" });
-                     }
+                     return Ok(new { transaction = newTransaction });
                   }
+                  return BadRequest(new { error = "Failed to retrieve the user!" });
                }
-               return NotFound(new { error = "No Data Recive." });
             }
             return Unauthorized(new { error = "Invalid Token!" });
          }
          return Unauthorized(new { error = "No token!" });
       }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
+      }
       catch (Exception ex)
       {
          Console.WriteLine($"[Server] Error: {ex.Message}");
-         return BadRequest(new { error = ex.Message });
+         return StatusCode(500, new { error = ex.Message });
       }
    }
 
-   [HttpPatch]
-   public async Task<ActionResult<TransactionModel>> UpdateTransaction(TransactionModel updatedTransaction)
+   [HttpPatch()]
+   public async Task<ActionResult<TransactionModel>> UpdateTransaction([FromBody] TransactionModel updatedTransaction)
    {
       try
       {
@@ -127,66 +286,80 @@ public class TransactionController : ControllerBase
 
             if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-               if (updatedTransaction != null)
+               using (var context = new EFDataContext())
                {
-                  if (updatedTransaction.TransactionID != 0 && updatedTransaction.Description != null && updatedTransaction.Value != 0 && updatedTransaction.Type != null && updatedTransaction.Date != null && updatedTransaction.FkCategory != 0)
+                  var dbTransaction = await context.Transactions.FirstOrDefaultAsync(t => t.TransactionID == updatedTransaction.TransactionID);
+
+                  if (dbTransaction != null && dbTransaction.FkUser == id)
                   {
-                     using (var context = new EFDataContext())
-                     {
-                        var dbTransaction = await context.Transactions.FindAsync(updatedTransaction.TransactionID);
+                     dbTransaction.TransactionID = updatedTransaction.TransactionID;
+                     dbTransaction.Description = updatedTransaction.Description;
+                     dbTransaction.Value = updatedTransaction.Value;
+                     dbTransaction.Type = updatedTransaction.Type;
+                     dbTransaction.Date = updatedTransaction.Date;
+                     dbTransaction.FkCategory = updatedTransaction.FkCategory;
 
-                        if (dbTransaction != null && dbTransaction.FkUser == id)
-                        {
-                           dbTransaction.Description = updatedTransaction.Description;
-                           dbTransaction.Value = updatedTransaction.Value;
-                           dbTransaction.Type = updatedTransaction.Type;
-                           dbTransaction.Date = updatedTransaction.Date;
-                           dbTransaction.FkCategory = updatedTransaction.FkCategory;
+                     await context.SaveChangesAsync();
 
-                           await context.SaveChangesAsync();
-
-                           return Ok(dbTransaction);
-                        }
-                        return BadRequest(new { error = "Failed!" });
-                     }
+                     return Ok(new { transaction = dbTransaction });
                   }
+                  return NotFound(new { error = "No Transaction found." });
                }
-               return NotFound(new { error = "No Data Recive." });
             }
             return Unauthorized(new { error = "Invalid Token!" });
          }
          return BadRequest(new { error = "No token!" });
       }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
+      }
       catch (Exception ex)
       {
          Console.WriteLine($"[Server] Error: {ex.Message}");
-         return BadRequest(new { error = ex.Message });
+         return StatusCode(500, new { error = ex.Message });
       }
    }
 
-   [HttpDelete]
-   public async Task<ActionResult<TransactionModel>> DeleteTransaction(int id)
+   [HttpDelete("{transactionId}")]
+   public async Task<ActionResult<TransactionModel>> DeleteTransaction(long transactionId)
    {
       try
       {
-         using (var context = new EFDataContext())
+         if (HttpContext.Request.Headers["Authorization"] != String.Empty)
          {
-            var transaction = await context.Transactions.FindAsync(id);
-            if (transaction == null)
+            var token = HttpContext.Request.Headers["Authorization"];
+
+            var jwtToken = new JwtSecurityToken(token);
+            var payload = jwtToken.Payload;
+
+            if (payload.TryGetValue("id", out object? idObj) && long.TryParse(idObj.ToString(), out long id))
             {
-               return NotFound();
+               using (var context = new EFDataContext())
+               {
+                  var dbTransaction = await context.Transactions.FirstOrDefaultAsync(t => t.TransactionID == transactionId);
+
+                  if (dbTransaction != null && dbTransaction.FkUser == id)
+                  {
+                     context.Transactions.Remove(dbTransaction);
+                     await context.SaveChangesAsync();
+                     return Ok();
+                  }
+                  return NotFound(new { error = "No Transaction found." });
+               }
             }
-
-            context.Transactions.Remove(transaction);
-            await context.SaveChangesAsync();
-
-            return Ok(transaction);
+            return Unauthorized(new { error = "Invalid Token!" });
          }
+         return BadRequest(new { error = "No token!" });
+      }
+      catch (ArgumentException ex)
+      {
+         return BadRequest(new { error = ex.Message });
       }
       catch (Exception ex)
       {
          Console.WriteLine($"[Server] Error: {ex.Message}");
-         return BadRequest(new { error = ex.Message });
+         return StatusCode(500, new { error = ex.Message });
       }
    }
 }
